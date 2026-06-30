4767a3127a401f8d77ec37ea7d13568f14a69ab5
apdizalam <apdizalammohameth@gmail.com>
2026-06-30 18:21:54 +0300

    Update API_BASE to use production URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE = 'http://localhost:5000/api';
+export const API_BASE = 'https://backend-q2g2.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
